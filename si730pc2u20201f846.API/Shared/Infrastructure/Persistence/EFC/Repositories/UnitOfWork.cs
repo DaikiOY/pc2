f0c0f02b2f47c77ead4ee70532125e79262a527d
DaikiOY <167911449+DaikiOY@users.noreply.github.com>
2024-11-06 19:43:38 -0500

    Create UnitOfWork.cs

---
+++ si730pc2u20201f846.API/Shared/Infrastructure/Persistence/EFC/Repositories/UnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using si730pc2u20201f846.API.Shared.Domain.Repositories;
+using si730pc2u20201f846.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace si730pc2u20201f846.API.Shared.Infrastructure.Persistence.EFC.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CompleteAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}

269d5ce034ca9a13082874af0113132831ac9f37
DaikiOY <167911449+DaikiOY@users.noreply.github.com>
2024-11-06 20:31:39 -0500

    Create ProductRepository.cs

---
+++ si730pc2u20201f846.API/WMS/Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using si730pc2u20201f846.Domain.Entities;
+using si730pc2u20201f846.Infrastructure.Persistence;
+
+namespace si730pc2u20201f846.Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _context;
+
+        public ProductRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync()
+        {
+            return await _context.Products.ToListAsync();
+        }
+
+        public async Task<Product> GetByIdAsync(Guid productId)
+        {
+            return await _context.Products.FindAsync(productId);
+        }
+
+        public async Task AddAsync(Product product)
+        {
+            await _context.Products.AddAsync(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Product product)
+        {
+            _context.Products.Update(product);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Product product)
+        {
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+        }
+    }
+}

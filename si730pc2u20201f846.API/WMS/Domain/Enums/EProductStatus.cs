78f661b36355b573ce8feb00bb89104517c7386c
DaikiOY <167911449+DaikiOY@users.noreply.github.com>
2024-11-06 20:25:16 -0500

    Create EProductStatus.cs

---
+++ si730pc2u20201f846.API/WMS/Domain/Enums/EProductStatus.cs
@@ -0,0 +1,11 @@
+namespace si730pc2u20201f846.Domain.Enums
+{
+    public enum EProductStatus
+    {
+        Available = 1,
+        OutOfStock = 2,
+        Discontinued = 3,
+        BackOrder = 4,
+        Returned = 5
+    }
+}

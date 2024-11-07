8075bf4f5ed7b446a21c9461783ac6e4713a0843
DaikiOY <167911449+DaikiOY@users.noreply.github.com>
2024-11-06 20:22:47 -0500

    Create Product.cs

---
+++ si730pc2u20201f846.API/WMS/Domain/Entities/Product.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace si730pc2u20201f846.Domain.Entities
+{
+    public class Product
+    {
+        public Guid ProductId { get; private set; } = Guid.NewGuid();
+        public string ProductName { get; set; }
+        public string Sku { get; set; }
+        public int CategoryId { get; set; }
+        public int Status { get; set; }
+        public int Quantity { get; set; } = 0;
+        public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+
+        // Constructor simplificado
+        public Product(string productName, string sku, int categoryId, int status)
+        {
+            ProductName = productName;
+            Sku = sku;
+            CategoryId = categoryId;
+            Status = status;
+        }
+    }
+}

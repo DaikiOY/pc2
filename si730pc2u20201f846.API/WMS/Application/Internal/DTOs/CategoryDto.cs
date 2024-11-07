e36e6313e3802fe2b47f602a3d262d52dd4a6ad5
DaikiOY <167911449+DaikiOY@users.noreply.github.com>
2024-11-06 20:04:20 -0500

    Create CategoryDto.cs

---
+++ si730pc2u20201f846.API/WMS/Application/Internal/DTOs/CategoryDto.cs
@@ -0,0 +1,11 @@
+namespace si730pc2u20201f846.API.WMS.Application.Internal.DTOs
+{
+    /// <summary>
+    /// Data Transfer Object for Category.
+    /// </summary>
+    public class CategoryDto
+    {
+        public int Id { get; set; } // Unique identifier for the category
+        public string Name { get; set; } // Name of the category
+    }
+}

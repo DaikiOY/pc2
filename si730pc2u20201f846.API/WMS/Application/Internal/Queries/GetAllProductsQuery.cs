6e20d886a44d1f33edefd8a1028c4e04574751db
DaikiOY <167911449+DaikiOY@users.noreply.github.com>
2024-11-06 20:10:48 -0500

    Create GetAllProductsQuery.cs

---
+++ si730pc2u20201f846.API/WMS/Application/Internal/Queries/GetAllProductsQuery.cs
@@ -0,0 +1,10 @@
+namespace si730pc2u20201f846.API.WMS.Application.Internal.Queries
+{
+    /// <summary>
+    /// Query to retrieve all products.
+    /// </summary>
+    public class GetAllProductsQuery
+    {
+
+    }
+}
